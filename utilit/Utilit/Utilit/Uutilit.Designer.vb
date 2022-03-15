a0f13e1de06a85db5886e76ac3c41a3ff7d4045f
pinguuuuuu <50883179+pinguuuuuu@users.noreply.github.com>
2022-03-15 16:45:17 +0100

    Add files via upload

---
+++ utilit/Utilit/Utilit/Uutilit.Designer.vb
@@ -0,0 +1,376 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Uutilit
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Process1 = New System.Diagnostics.Process()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Process2 = New System.Diagnostics.Process()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.Button17 = New System.Windows.Forms.Button()
+        Me.Button18 = New System.Windows.Forms.Button()
+        Me.Button19 = New System.Windows.Forms.Button()
+        Me.Button20 = New System.Windows.Forms.Button()
+        Me.Button21 = New System.Windows.Forms.Button()
+        Me.Button22 = New System.Windows.Forms.Button()
+        Me.Button23 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 29)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 40)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Table de Caractère"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Process1
+        '
+        Me.Process1.StartInfo.Domain = ""
+        Me.Process1.StartInfo.LoadUserProfile = False
+        Me.Process1.StartInfo.Password = Nothing
+        Me.Process1.StartInfo.StandardErrorEncoding = Nothing
+        Me.Process1.StartInfo.StandardOutputEncoding = Nothing
+        Me.Process1.StartInfo.UserName = ""
+        Me.Process1.SynchronizingObject = Me
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(216, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "-------------------------UTILITAIRE-------------------------"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(122, 29)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(100, 40)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Calculatrice"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Process2
+        '
+        Me.Process2.StartInfo.Domain = ""
+        Me.Process2.StartInfo.LoadUserProfile = False
+        Me.Process2.StartInfo.Password = Nothing
+        Me.Process2.StartInfo.StandardErrorEncoding = Nothing
+        Me.Process2.StartInfo.StandardOutputEncoding = Nothing
+        Me.Process2.StartInfo.UserName = ""
+        Me.Process2.SynchronizingObject = Me
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 75)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 40)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Loupe"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(122, 75)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(100, 40)
+        Me.Button4.TabIndex = 7
+        Me.Button4.Text = "Clavier (versions)"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 114)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(208, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "-------------------------GESTION--------------------------"
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(12, 130)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(100, 40)
+        Me.Button5.TabIndex = 10
+        Me.Button5.Text = "Gestion PC"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(122, 130)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(100, 40)
+        Me.Button6.TabIndex = 11
+        Me.Button6.Text = "Panneau de configuration"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(12, 176)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(100, 40)
+        Me.Button7.TabIndex = 12
+        Me.Button7.Text = "Gestion Peripherique"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(122, 176)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(100, 40)
+        Me.Button8.TabIndex = 13
+        Me.Button8.Text = "Gestion du son"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(12, 222)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(100, 40)
+        Me.Button9.TabIndex = 14
+        Me.Button9.Text = "Information système"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(122, 222)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(100, 40)
+        Me.Button10.TabIndex = 15
+        Me.Button10.Text = "Gestionnaire de tâche"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(12, 268)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(100, 40)
+        Me.Button11.TabIndex = 16
+        Me.Button11.Text = "Version Windows"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(123, 268)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(100, 40)
+        Me.Button12.TabIndex = 17
+        Me.Button12.Text = "Gestion affichage"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(9, 311)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(209, 13)
+        Me.Label3.TabIndex = 18
+        Me.Label3.Text = "-------------------------CONTROL-------------------------"
+        '
+        'Button13
+        '
+        Me.Button13.Location = New System.Drawing.Point(12, 327)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(100, 40)
+        Me.Button13.TabIndex = 19
+        Me.Button13.Text = "Outils administration"
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'Button14
+        '
+        Me.Button14.Location = New System.Drawing.Point(123, 327)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(100, 40)
+        Me.Button14.TabIndex = 20
+        Me.Button14.Text = "Controle des dossiers"
+        Me.Button14.UseVisualStyleBackColor = True
+        '
+        'Button15
+        '
+        Me.Button15.Location = New System.Drawing.Point(12, 373)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(100, 40)
+        Me.Button15.TabIndex = 21
+        Me.Button15.Text = "Région date et heure"
+        Me.Button15.UseVisualStyleBackColor = True
+        '
+        'Button16
+        '
+        Me.Button16.Location = New System.Drawing.Point(123, 373)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(100, 40)
+        Me.Button16.TabIndex = 22
+        Me.Button16.Text = "Controle clavier et souris"
+        Me.Button16.UseVisualStyleBackColor = True
+        '
+        'Button17
+        '
+        Me.Button17.Location = New System.Drawing.Point(12, 419)
+        Me.Button17.Name = "Button17"
+        Me.Button17.Size = New System.Drawing.Size(100, 40)
+        Me.Button17.TabIndex = 23
+        Me.Button17.Text = "Controle des composants externes"
+        Me.Button17.UseVisualStyleBackColor = True
+        '
+        'Button18
+        '
+        Me.Button18.Location = New System.Drawing.Point(122, 419)
+        Me.Button18.Name = "Button18"
+        Me.Button18.Size = New System.Drawing.Size(100, 40)
+        Me.Button18.TabIndex = 24
+        Me.Button18.Text = "Compte et MotDePasse"
+        Me.Button18.UseVisualStyleBackColor = True
+        '
+        'Button19
+        '
+        Me.Button19.Location = New System.Drawing.Point(12, 465)
+        Me.Button19.Name = "Button19"
+        Me.Button19.Size = New System.Drawing.Size(100, 40)
+        Me.Button19.TabIndex = 25
+        Me.Button19.Text = "Controle des Evenement"
+        Me.Button19.UseVisualStyleBackColor = True
+        '
+        'Button20
+        '
+        Me.Button20.Location = New System.Drawing.Point(123, 465)
+        Me.Button20.Name = "Button20"
+        Me.Button20.Size = New System.Drawing.Size(100, 40)
+        Me.Button20.TabIndex = 26
+        Me.Button20.Text = "Controle de la manette"
+        Me.Button20.UseVisualStyleBackColor = True
+        '
+        'Button21
+        '
+        Me.Button21.Location = New System.Drawing.Point(12, 511)
+        Me.Button21.Name = "Button21"
+        Me.Button21.Size = New System.Drawing.Size(100, 40)
+        Me.Button21.TabIndex = 27
+        Me.Button21.Text = "Performance"
+        Me.Button21.UseVisualStyleBackColor = True
+        '
+        'Button22
+        '
+        Me.Button22.Location = New System.Drawing.Point(123, 511)
+        Me.Button22.Name = "Button22"
+        Me.Button22.Size = New System.Drawing.Size(100, 40)
+        Me.Button22.TabIndex = 28
+        Me.Button22.Text = "Services (admin)"
+        Me.Button22.UseVisualStyleBackColor = True
+        '
+        'Button23
+        '
+        Me.Button23.Location = New System.Drawing.Point(60, 557)
+        Me.Button23.Name = "Button23"
+        Me.Button23.Size = New System.Drawing.Size(100, 40)
+        Me.Button23.TabIndex = 29
+        Me.Button23.Text = "Transfert Bluetooth"
+        Me.Button23.UseVisualStyleBackColor = True
+        '
+        'Uutilit
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.ClientSize = New System.Drawing.Size(235, 618)
+        Me.Controls.Add(Me.Button23)
+        Me.Controls.Add(Me.Button22)
+        Me.Controls.Add(Me.Button21)
+        Me.Controls.Add(Me.Button20)
+        Me.Controls.Add(Me.Button19)
+        Me.Controls.Add(Me.Button18)
+        Me.Controls.Add(Me.Button17)
+        Me.Controls.Add(Me.Button16)
+        Me.Controls.Add(Me.Button15)
+        Me.Controls.Add(Me.Button14)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Uutilit"
+        Me.Text = "Utilit"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Process1 As System.Diagnostics.Process
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Process2 As System.Diagnostics.Process
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+    Friend WithEvents Button10 As System.Windows.Forms.Button
+    Friend WithEvents Button11 As System.Windows.Forms.Button
+    Friend WithEvents Button12 As System.Windows.Forms.Button
+    Friend WithEvents Button13 As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Button14 As System.Windows.Forms.Button
+    Friend WithEvents Button15 As System.Windows.Forms.Button
+    Friend WithEvents Button16 As System.Windows.Forms.Button
+    Friend WithEvents Button17 As System.Windows.Forms.Button
+    Friend WithEvents Button18 As System.Windows.Forms.Button
+    Friend WithEvents Button19 As System.Windows.Forms.Button
+    Friend WithEvents Button20 As System.Windows.Forms.Button
+    Friend WithEvents Button21 As System.Windows.Forms.Button
+    Friend WithEvents Button22 As System.Windows.Forms.Button
+    Friend WithEvents Button23 As System.Windows.Forms.Button
+
+End Class
---
+++ utilit/Utilit/Utilit/Uutilit.vb
@@ -0,0 +1,233 @@
+﻿Public Class Uutilit
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        Dim p As New Process
+
+        p.StartInfo.FileName = "cmd.exe"
+        p.StartInfo.Arguments = "/c charmap"
+        p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        p.Start()
+
+    End Sub
+
+    Private Sub Uutilit_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+
+        Dim c As New Process
+
+        c.StartInfo.FileName = "cmd.exe"
+        c.StartInfo.Arguments = "/c calc"
+        c.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        c.Start()
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+
+        Dim d As New Process
+
+        d.StartInfo.FileName = "cmd.exe"
+        d.StartInfo.Arguments = "/c MAGNIFY"
+        d.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        d.Start()
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        Dim f As New Process
+
+        f.StartInfo.FileName = "cmd.exe"
+        f.StartInfo.Arguments = "/c OSK"
+        f.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        f.Start()
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+
+
+        Dim y As New Process
+
+        y.StartInfo.FileName = "cmd.exe"
+        y.StartInfo.Arguments = "/c COMPMGMT.MSC"
+        y.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        y.Start()
+    End Sub
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        Dim i As New Process
+
+        i.StartInfo.FileName = "cmd.exe"
+        i.StartInfo.Arguments = "/c control"
+        i.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        i.Start()
+    End Sub
+
+    Private Sub Button7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button7.Click
+        Dim i As New Process
+
+        i.StartInfo.FileName = "cmd.exe"
+        i.StartInfo.Arguments = "/c DEVMGMT.MSC "
+        i.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        i.Start()
+    End Sub
+
+    Private Sub Button8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button8.Click
+        Dim i As New Process
+
+        i.StartInfo.FileName = "cmd.exe"
+        i.StartInfo.Arguments = "/c MMSYS.CPL  "
+        i.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        i.Start()
+    End Sub
+
+    Private Sub Button9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button9.Click
+        Dim i As New Process
+
+        i.StartInfo.FileName = "cmd.exe"
+        i.StartInfo.Arguments = "/c  MSINFO32 "
+        i.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        i.Start()
+    End Sub
+
+    Private Sub Button10_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button10.Click
+        Dim i As New Process
+
+        i.StartInfo.FileName = "cmd.exe"
+        i.StartInfo.Arguments = "/c  TASKMGR"
+        i.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        i.Start()
+    End Sub
+
+    Private Sub Button11_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button11.Click
+        Dim i As New Process
+
+        i.StartInfo.FileName = "cmd.exe"
+        i.StartInfo.Arguments = "/c  WINVER"
+        i.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        i.Start()
+    End Sub
+
+    Private Sub Button12_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button12.Click
+        Dim i As New Process
+
+        i.StartInfo.FileName = "cmd.exe"
+        i.StartInfo.Arguments = "/c  DESK.CPL"
+        i.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        i.Start()
+    End Sub
+
+    Private Sub Button13_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button13.Click
+        Dim i As New Process
+
+        i.StartInfo.FileName = "cmd.exe"
+        i.StartInfo.Arguments = "/c  CONTROL ADMINTOOLS"
+        i.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        i.Start()
+    End Sub
+
+    Private Sub Button15_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button15.Click
+        Dim i As New Process
+
+        i.StartInfo.FileName = "cmd.exe"
+        i.StartInfo.Arguments = "/c  CONTROL INTERNATIONAL"
+        i.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        i.Start()
+    End Sub
+
+    Private Sub Button14_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button14.Click
+        Dim i As New Process
+
+        i.StartInfo.FileName = "cmd.exe"
+        i.StartInfo.Arguments = "/c  CONTROL FOLDERS"
+        i.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        i.Start()
+    End Sub
+
+    Private Sub Button16_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button16.Click
+        Dim i As New Process
+
+        i.StartInfo.FileName = "cmd.exe"
+        i.StartInfo.Arguments = "/c  CONTROL MOUSE & CONTROL KEYBOARD"
+        i.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        i.Start()
+    End Sub
+
+    Private Sub Button17_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button17.Click
+        Dim i As New Process
+
+        i.StartInfo.FileName = "cmd.exe"
+        i.StartInfo.Arguments = "/c  CONTROL PRINTERS"
+        i.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        i.Start()
+    End Sub
+
+    Private Sub Button18_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button18.Click
+        Dim i As New Process
+
+        i.StartInfo.FileName = "cmd.exe"
+        i.StartInfo.Arguments = "/c  CONTROl USERPASSWORDS"
+        i.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        i.Start()
+    End Sub
+
+    Private Sub Button19_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button19.Click
+        Dim i As New Process
+
+        i.StartInfo.FileName = "cmd.exe"
+        i.StartInfo.Arguments = "/c EVENTVWR"
+        i.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        i.Start()
+    End Sub
+
+    Private Sub Button20_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button20.Click
+        Dim i As New Process
+
+        i.StartInfo.FileName = "cmd.exe"
+        i.StartInfo.Arguments = "/c  JOY.CPL"
+        i.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        i.Start()
+    End Sub
+
+    Private Sub Button21_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button21.Click
+        Dim i As New Process
+
+        i.StartInfo.FileName = "cmd.exe"
+        i.StartInfo.Arguments = "/c  PERFMON"
+        i.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        i.Start()
+    End Sub
+
+    Private Sub Button22_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button22.Click
+        Dim i As New Process
+
+        i.StartInfo.FileName = "cmd.exe"
+        i.StartInfo.Arguments = "/c  SERVICES.MSC"
+        i.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        i.Start()
+    End Sub
+
+    Private Sub Button23_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button23.Click
+        Dim i As New Process
+
+        i.StartInfo.FileName = "cmd.exe"
+        i.StartInfo.Arguments = "/c FSQUIRT"
+        i.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        i.Start()
+    End Sub
+
+    Private Sub Button24_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Dim i As New Process
+
+        i.StartInfo.FileName = "cmd.exe"
+        i.StartInfo.Arguments = "/c  MSCTSC"
+        i.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        i.Start()
+    End Sub
+End Class
